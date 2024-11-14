ee9187a285722cb19a22a106692590eef0a39ec1
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-14 20:40:56 +0300

    Add files via upload

---
+++ KitapVeYazar.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+class Kitap
+{
+    public string Baslik { get; set; }
+    public string ISBN { get; set; }
+
+    public Kitap(string baslik, string isbn)
+    {
+        Baslik = baslik;
+        ISBN = isbn;
+    }
+}
+
+class Yazar
+{
+    public string Ad { get; set; }
+    public string Ulke { get; set; }
+    public List<Kitap> Kitaplar { get; set; }
+
+    public Yazar(string ad, string ulke)
+    {
+        Ad = ad;
+        Ulke = ulke;
+        Kitaplar = new List<Kitap>();
+    }
+
+    public void KitapEkle(Kitap kitap)
+    {
+        Kitaplar.Add(kitap);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Yazar ve Kitap Testi
+        Yazar yazar = new Yazar("Orhan Pamuk", "Türkiye");
+        Kitap kitap1 = new Kitap("Kar", "9789750507106");
+        Kitap kitap2 = new Kitap("Masumiyet Müzesi", "9789750507144");
+
+        yazar.KitapEkle(kitap1);
+        yazar.KitapEkle(kitap2);
+
+        Console.WriteLine(yazar.Ad + " adlı yazarın kitapları:");
+        foreach (var kitap in yazar.Kitaplar)
+        {
+            Console.WriteLine("- " + kitap.Baslik);
+        }
+    }
+}
